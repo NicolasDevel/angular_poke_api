e6bcffbe8c71358cc36588bfca44d371eaf6ad42
NicoDev <nicolassabogal85@gmail.com>
2025-06-18 20:58:10 -0500

    Basic conf

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Home } from './page/home/home';
+import { DetailPokemon } from './page/detail-pokemon/detail-pokemon';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: Home
+    },
+    {
+        path: 'detail',
+        component: DetailPokemon
+    }
+];
